17cacdc1855259821714548c1b755296f150ca66
Sevilla <fsevilla@gmail.com>
2020-10-08 22:38:13 -0500

    updated env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'https://jsonplaceholder.typicode.com/',
-  clientId: '89e9b028301e4478a60edca06988d1f6',
-  clientSecret: 'e27942e47d464f51b60a81a9a1fef1c7',
+  clientId: '',
+  clientSecret: '',
   accountSpotifyUrl: 'https://accounts.spotify.com/',
   spotifyUrl: 'https://api.spotify.com/v1/',
   basePath: 'http://localhost:4200/'
